9eae779eb25eabda74c1b38de4e42a9b579aa982
Hillary Prather <prathh@uw.edu>
2015-01-27 13:09:36 -0800

    Completed button functionality

--- app/src/main/java/edu/washington/prathh/lifecounter/MainActivity.java
+++ app/src/main/java/edu/washington/prathh/lifecounter/MainActivity.java
@@ -2,33 +2,103 @@ package edu.washington.prathh.lifecounter;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-    private ListView list;
-    private ArrayAdapter<String> listAdapter;
+    private static int playerOneCount;
+    private static int playerTwoCount;
+    private static int playerThreeCount;
+    private static int playerFourCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list = (ListView) findViewById(R.id.listView);
-        List<String> players = new ArrayList<String>();
-        players.add("Player 1");
-        players.add("Player 2");
-        players.add("Player 3");
-        players.add("Player 4");
-        listAdapter = new ArrayAdapter<String>(this, R.layout.player, players);
-        list.setAdapter(listAdapter);
     }
 
+    public void button(View v) {
+        switch (v.getId()) {
+            case R.id.playOneMinusFive:
+                playerOneCount -= 5;
+                changeScore(findViewById(R.id.oneString), "Player 1", playerOneCount);
+                break;
+            case R.id.playOneMinusOne:
+                playerOneCount--;
+                changeScore(findViewById(R.id.oneString), "Player 1", playerOneCount);
+                break;
+            case R.id.playOnePlusFive:
+                playerOneCount += 5;
+                changeScore(findViewById(R.id.oneString), "Player 1", playerOneCount);
+                break;
+            case R.id.playOnePlusOne:
+                playerOneCount++;
+                changeScore(findViewById(R.id.oneString), "Player 1", playerOneCount);
+                break;
+            case R.id.playTwoMinusFive:
+                playerTwoCount -= 5;
+                changeScore(findViewById(R.id.twoString), "Player 2", playerTwoCount);
+                break;
+            case R.id.playTwoMinusOne:
+                playerTwoCount--;
+                changeScore(findViewById(R.id.twoString), "Player 2", playerTwoCount);
+                break;
+            case R.id.playTwoPlusFive:
+                playerTwoCount += 5;
+                changeScore(findViewById(R.id.twoString), "Player 2", playerTwoCount);
+                break;
+            case R.id.playTwoPlusOne:
+                playerTwoCount++;
+                changeScore(findViewById(R.id.twoString), "Player 2", playerTwoCount);
+                break;
+            case R.id.playThreeMinusFive:
+                playerThreeCount -= 5;
+                changeScore(findViewById(R.id.threeString), "Player 3", playerThreeCount);
+                break;
+            case R.id.playThreeMinusOne:
+                playerThreeCount--;
+                changeScore(findViewById(R.id.threeString), "Player 3", playerThreeCount);
+                break;
+            case R.id.playThreePlusFive:
+                playerThreeCount += 5;
+                changeScore(findViewById(R.id.threeString), "Player 3", playerThreeCount);
+                break;
+            case R.id.playThreePlusOne:
+                playerThreeCount++;
+                changeScore(findViewById(R.id.threeString), "Player 3", playerThreeCount);
+                break;
+            case R.id.playFourMinusFive:
+                playerFourCount -= 5;
+                changeScore(findViewById(R.id.fourString), "Player 4", playerFourCount);
+                break;
+            case R.id.playFourMinusOne:
+                playerFourCount--;
+                changeScore(findViewById(R.id.fourString), "Player 4", playerFourCount);
+                break;
+            case R.id.playFourPlusFive:
+                playerFourCount += 5;
+                changeScore(findViewById(R.id.fourString), "Player 4", playerFourCount);
+                break;
+            case R.id.playFourPlusOne:
+                playerFourCount++;
+                changeScore(findViewById(R.id.fourString), "Player 4", playerFourCount);
+                break;
+        }
+    }
+
+    public void changeScore(View view, String player, int score) {
+        TextView text = (TextView) view;
+        text.setText(player + ": " + score + " Lives");
+        if (score <= 0) {
+            ((TextView) findViewById(R.id.loser)).setText(player + " Loses!");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
